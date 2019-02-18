3b3bf295009bf29eb4131fb4b834e3f1ad4d9c7d
xxKira <43138477+xxKira@users.noreply.github.com>
2019-02-18 19:22:39 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -34,3 +34,8 @@ elseif(strpos($text, "/data") === 0)
 {
 	$response = " Sono le ore $date" ;
 }
+
+header("Content-Type: application/json");
+$parameters = array('chat_id' => $chatId, "text" => $response);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);
